c852cd4b77ad5b16571f05e3e77a2b139c5e80e9
Achutendra Singh <achu1997singh@gmail.com>
2022-09-30 14:39:18 +0530

    added teams route and controller

--- routes/teams/teams.controller.js
+++ routes/teams/teams.controller.js
@@ -1,5 +1,56 @@
-function getTeams(req, res) {
-  res.status(200).json({ message: 'Testing' })
+const { default: mongoose } = require("mongoose")
+const { Teams } = require("../../model/teams")
+
+
+async function getTeamsRequest(req, res) {
+  try{
+    // console.log("this is team get request")
+    const teams = await Teams.find()
+    res.json(teams)
+  }catch(err){
+    res.send('Error' + err)
+  }
 }
 
-module.exports = { getTeams }
+async function getByIdTeamRequest(req, res){
+  try{
+    const team = await Teams.findById(req.param.id)
+    if(team){
+      res.json(team)
+    }
+   
+  }catch(err){
+    res.send('Error' + err)
+  }
+}
+
+
+async function postTeamsRequest(req, res){
+  const team = new Teams({
+    _id: new mongoose.Types.ObjectId,
+    name: req.body.name,
+    description: req.body.description,
+    createdBy: req.body.createdBy,
+    updatedBy: req.body.updatedBy,
+  })
+
+  try{
+    const result = await Teams.save()
+    res.json(result._id)
+  }catch(err){
+    res.send('Error' + err)
+  }
+}
+
+async function deleteTeamsRequest(req, res){
+  try{
+    const team = await Teams.findById(req.param.id)
+    const result = await team.remove()
+    res.json(result)
+  }catch(err){
+    res.send('Error' + err)
+  }
+}
+
+
+module.exports = { getTeamsRequest, postTeamsRequest, deleteTeamsRequest, getByIdTeamRequest }
